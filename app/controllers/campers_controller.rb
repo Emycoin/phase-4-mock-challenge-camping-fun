7447d787b61ce60a028053c4d3045b1eccf7d7d9
Emycoin <emytausevich@gmail.com>
2021-09-04 19:06:21 -0400

    completed

---
+++ app/controllers/campers_controller.rb
@@ -0,0 +1,29 @@
+class CampersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_message
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid_message
+  def index
+    render json: Camper.all
+  end
+  def show
+    camper = Camper.find(params[:id])
+    render json: camper
+  end
+
+  def create
+    camper = Camper.create(camper_params)
+    render json: camper, status: :created
+end
+
+  private
+
+  def camper_params
+    params.permit(:name, :age)
+  end
+
+  def record_not_found_message(exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
+  end
+  def record_invalid_message(invalid)
+    render json: { error: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+end
